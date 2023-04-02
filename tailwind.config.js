210d0ea20eb54f6eff2a9d7ebf5256575bda6ae5
Landensaki <gededewowahyumw@gmail.com>
2023-04-02 22:14:07 +0700

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/components/**/*.js", "./public/index.html"],
+  content: ["./src/components/**/*.js", "./src/App.js"],
   theme: {
     extend: {},
   },
