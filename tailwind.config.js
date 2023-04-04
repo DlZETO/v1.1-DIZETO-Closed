dbb7c6cb7f30749f19468b50f294dd18b6b723b8
Landensaki <gededewowahyumw@gmail.com>
2023-04-04 17:01:48 +0700

    Refresh Delete

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/components/**/*.jsx", "./src/App.jsx", "./src/pages/*.jsx"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
